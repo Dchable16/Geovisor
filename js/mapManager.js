24e896ab91833b2142ffb4a4d9226d8da7bbe3b8
Dchable16 <dcm.chable89@gmail.com>
2025-10-15 13:25:54 -0600

    Change draw control position from topleft to topcenter

--- js/mapManager.js
+++ js/mapManager.js
@@ -36,7 +36,7 @@ export class MapManager {
 
     addDrawControl() {
         const drawControl = new L.Control.Draw({
-            position: 'topleft',
+            position: 'topcenter',
             edit: {
                 featureGroup: this.drawnItems, 
                 remove: true 
