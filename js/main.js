dda038102a2304e3c7746de409ddefe43d9106c4
Diego_Chab <dcm.chable89@gmail.com>
2025-11-21 17:09:00 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -66,7 +66,6 @@ class GeovisorApp {
             hydraulicProps: {},   // Base de datos JSON
             wellsData: null
         };
-
         this.lastFilteredAquifer = 'NINGUNO';
 
         /**
@@ -110,11 +109,6 @@ class GeovisorApp {
             this.uiManager.refreshControls(this.data.vulnNames, this.data.vulnKeyMap);
             this.render();
             return;
-        }
-        
-        if (newState.selectedAquifer !== undefined && newState.selectedAquifer !== this.state.selectedAquifer) {
-            // Si cambia el acuífero, forzamos la deselección del pozo
-            newState.selectedWellId = null; 
         }
 
         // 2. Navegación
@@ -171,11 +165,8 @@ class GeovisorApp {
      */
     async init() {
         this.uiManager.setLoading(true);
-        
-        const map = this.mapManager.map;
-        map.createPane('wellsPane');
-        map.getPane('wellsPane').style.zIndex = 600; // Piso superior
-        
+
+        this.uiManager.setLoading(false);
         // 1. Carga de base de datos hidráulica con estrategia de fallback (redilencia)
         let hydroData = null;
         const pathsToTry = [
@@ -329,20 +320,17 @@ class GeovisorApp {
         
         if (wellsData) {
             console.log(`✅ Pozos cargados: ${wellsData.features.length} registros.`);
+            
+            // A. Guardamos los datos crudos en memoria
             this.data.wellsData = wellsData; 
 
+            // B. Inicializamos la capa VACÍA (null) pero configurada
             this.leafletLayers.wells = L.geoJson(null, {
-                // OJO: Esto asigna el pane al contenedor, pero a veces los puntos SVG necesitan ayuda
-                pane: 'wellsPane', 
-                
-                pointToLayer: (feature, latlng) => L.circleMarker(latlng, {
-                    ...this.getWellStyle(feature),
-                }),
-                
+                pointToLayer: (feature, latlng) => L.circleMarker(latlng, this.getWellStyle(feature)),
                 onEachFeature: (feature, layer) => this.onWellFeature(feature, layer)
             });
         }
-    }    
+    }
     /**
      * Normaliza la clave del acuífero para asegurar coincidencia con la base de datos.
      * Convierte a string y rellena con ceros a la izquierda hasta 4 dígitos (ej: "201" -> "0201").
@@ -595,7 +583,7 @@ class GeovisorApp {
                     const nombre = (data ? data.nombre : null) || l.feature.properties.NOM_ACUIF || l.feature.properties.NOM_ACUI;
                     
                     if (this.state.selectedAquifer === nombre) {
-                        l.bringToFront(); 
+                        l.bringToFront();
                     }
                 });
             }
